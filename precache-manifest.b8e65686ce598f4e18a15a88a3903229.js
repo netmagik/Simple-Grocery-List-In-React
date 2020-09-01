e07e9746b37e94ed258b4a22db07750d276b68e6
Irina <irina@netmagik.com>
2020-09-01 12:59:32 -0400

    Updates

---
+++ precache-manifest.b8e65686ce598f4e18a15a88a3903229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e88ee502464f190446a0a02bc46d7da1",
+    "url": "/Simple-Grocery-List-In-React/index.html"
+  },
+  {
+    "revision": "e137990ed39eaa9a7665",
+    "url": "/Simple-Grocery-List-In-React/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "27477b9fe7e698222a3b",
+    "url": "/Simple-Grocery-List-In-React/static/js/2.bdae2990.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Simple-Grocery-List-In-React/static/js/2.bdae2990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e137990ed39eaa9a7665",
+    "url": "/Simple-Grocery-List-In-React/static/js/main.1ae018be.chunk.js"
+  },
+  {
+    "revision": "2b08bff79a0ce6bbf168",
+    "url": "/Simple-Grocery-List-In-React/static/js/runtime-main.6c11ded2.js"
+  }
+]);
