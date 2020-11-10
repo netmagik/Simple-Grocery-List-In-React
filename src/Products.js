27b7e33be7d6db8744e1bb254ce876b65b724e98
Irina <irina@netmagik.com>
2020-11-10 13:51:53 -0500

    Convert Products into stateless functional componenet

--- src/Products.js
+++ src/Products.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
-class Products extends React.Component {
-    render() {
-        const { name, quantity } = this.props.details;
-        return (
+const Products = props => {
+    const { name, quantity } = props.details;
+    return (
            <li>
               <h3>{name} - {quantity}</h3>
            </li>
-            
-        );
-    }
+    );
 }
 
 export default Products;
